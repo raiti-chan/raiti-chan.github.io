4ec3ff91ce64cb74f5335365cf7ee84d051f362b
Raiti <rusuke@t-net.ne.jp>
2016-12-20 00:33:26 +0900

    一部改変

--- mainMenuItemJs.js
+++ mainMenuItemJs.js
@@ -1,7 +1,8 @@
 $(function(){
 	
 	$('.menuItem').click(function(){
+		/** @namespace parent.main */
 		parent.main.location = jQuery(this).attr('data-url');
 	})
 
-})
+});
--- sidemenu.js
+++ sidemenu.js
@@ -1,24 +1,24 @@
-$(function(){
-$('#sidemenu_key').click(function(){
-    $('#menu_wrap').toggleClass("clicked");
-	$('#sidemenu_key_bar1').toggleClass("clicked");
-	$('#sidemenu_key_bar2').toggleClass("clicked");
-	$('#sidemenu_key_bar3').toggleClass("clicked");
-});
-var windowHeight = $(window).height();
-$("#sidemenu").height(windowHeight);
-var timer = false;
-$(window).resize(function() {
-	if (timer !== false) {
-		clearTimeout(timer);
-	}
-	timer = setTimeout(function() {
-		windowHeight = $(window).height();
-		$(sideMenu).height(windowHeight);
-	}, 50);
-});
+$(function () {
+	$('#sidemenu_key').click(function () {
+		$('#menu_wrap').toggleClass("clicked");
+		$('#sidemenu_key_bar1').toggleClass("clicked");
+		$('#sidemenu_key_bar2').toggleClass("clicked");
+		$('#sidemenu_key_bar3').toggleClass("clicked");
+	});
+	var windowHeight = $(window).height();
+	$("#sidemenu").height(windowHeight);
+	var timer = false;
+	$(window).resize(function () {
+		if (timer !== false) {
+			clearTimeout(timer);
+		}
+		timer = setTimeout(function () {
+			windowHeight = $(window).height();
+			$(sideMenu).height(windowHeight);
+		}, 50);
+	});
 
 
-/**************************/
-	
+	/**************************/
+
 });
