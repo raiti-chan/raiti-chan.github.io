29627e2d5d5a32f9e0efb2407f118ab203bd12a1
Raitichan <rusuke@t-net.ne.jp>
2019-09-05 13:05:03 +0900

    v 1.1

--- js/e-festa/auto.js
+++ js/e-festa/auto.js
@@ -9,16 +9,17 @@ function sleep(wait_ms) {
 function auto_run() {
 	if (event.ctrlKey != true) return;
 	alert("start");
-	sleep(5000);
-	var c_f = document.kdbox_iframe.document.ctrl;
-	var r_f = document.kdbox_iframe.document.right;
-	var test_size = c_f.testInfo.length;
-	for (var i = 0; i < test_size; i++) {
-		var ans = c_f.testInfo[i].correct.split("\n")[0];
-		r_f.test_info.answer = ans;
-		c_f.move_page(1);
-		c_f.move_page(1);
-	}
+	select_task();
+	//sleep(5000);
+	//var c_f = document.kdbox_iframe.document.ctrl;
+	//var r_f = document.kdbox_iframe.document.right;
+	//var test_size = c_f.testInfo.length;
+	//for (var i = 0; i < test_size; i++) {
+	//	var ans = c_f.testInfo[i].correct.split("\n")[0];
+	//	r_f.test_info.answer = ans;
+	//	c_f.move_page(1);
+	//	c_f.move_page(1);
+	//}
 }
 
 function select_task() {
