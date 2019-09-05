192ff35698e1b1c18fa34d3eada45c5263dd7aee
Raitichan <rusuke@t-net.ne.jp>
2019-09-05 15:47:08 +0900

    a

--- js/e-festa/auto.js
+++ js/e-festa/auto.js
@@ -20,7 +20,7 @@ function select_task() {
 function answer_task() {
 	var kdbox = document.kdbox_iframe;
 	kdbox.__doPostBack('ctl00$masterMain$dkgSubjectTop$hplStart','');
-	sleep(2);
+	await sleep(2);
 	var ctrl = kdbox.document.ctrl;
 	var right = kdbox_iframe.document.right;
 	switch (ctrl.test_type) {
