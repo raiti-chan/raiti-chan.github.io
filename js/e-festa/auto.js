c4a1816ae3aa64c5bce3156454f598365f4eff03
Raitichan <rusuke@t-net.ne.jp>
2019-09-05 16:28:55 +0900

    a

--- js/e-festa/auto.js
+++ js/e-festa/auto.js
@@ -10,10 +10,11 @@ function auto_run() {
 	select_task();
 }
 
-function select_task() {
+async function select_task() {
 	var list_f = document.list;
 	for (var i = 0; i < list_f.document.links.length; i++) {
 		list_f.location = list_f.document.links[i].href;
+		await sleep(1);
 		start();
 	}
 }
