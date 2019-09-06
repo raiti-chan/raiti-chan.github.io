b1272a2b45e69242434c13c50349e194abafc720
らいちちゃん <rusuke@t-net.ne.jp>
2019-09-06 09:25:01 +0900

    a -> e

--- js/e-festa/auto.js
+++ js/e-festa/auto.js
@@ -40,7 +40,7 @@ async function answer_task() {
 	await sleep(1);
 	var ctrl = document.ctrl;
 	var right = document.right;
-	ctrl.submitData._startDate.setSeconds(ctrl.submitData._startData.getSeconds() - (30 * ctrl.slides.length));
+	ctrl.submitData._startDate.setSeconds(ctrl.submitData._startDate.getSeconds() - (30 * ctrl.slides.length));
 	switch (ctrl.test_type) {
 		case 1 : //テキスト
 			for (var i = ctrl.currPage; i < ctrl.slides.length; i++) { 
